79754a964265afd55c2c9ee60b94656d8306c652
bladibap <bladimirbaptistagonzales@gmail.com>
2026-04-25 07:13:01 -0400

    test action

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,6 @@ cd /root/ms-kitchen
 
 git pull origin main
 
-DROPLET_IP=$(curl -s --max-time 5 http://159.89.227.37/metadata/v1/interfaces/public/0/ipv4/address)
-
-if [ -z "$DROPLET_IP" ]; then
-    echo "ERROR: No se pudo obtener DROPLET_IP"
-    exit 1
-fi
-
 if [ ! -f .env ]; then
     echo "ERROR: El archivo .env no existe en /root/ms-kitchen"
     exit 1
