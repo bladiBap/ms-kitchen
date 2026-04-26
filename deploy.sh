83ebb28581c99f888de72e21421431a92a1e1c00
bladibap <bladimirbaptistagonzales@gmail.com>
2026-04-26 12:11:34 -0400

    fixed: no reload database

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,3 @@ fi
 docker compose -f docker-compose.yml down
 
 docker compose -f docker-compose.yml up -d --build
-
-docker exec ms_kitchen_api npm run migration:generate
-
-docker exec ms_kitchen_api npm run migration:run
